6f078e42e394e144d76d698d8acbf1afdcfdba1e
Maky <franklinjaradev@gmail.com>
2021-05-31 22:32:39 -0400

    Header and font-awesome

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hi</h1>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to Bountifully</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 };
 
