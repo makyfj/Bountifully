b41e55e2fda8674e570e7d8631ff6350981c89f7
Maky <franklinjaradev@gmail.com>
2021-06-06 14:32:55 -0400

    Fetching Product Data from MongoDB

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express";
-import products from "./data/products.js";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
@@ -13,15 +13,8 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-
-  res.json(product);
-});
+// product routes
+app.use("/api/products", productRoutes);
 
 // dotenv
 const PORT = process.env.PORT || 5000;
