94cf30903a8c5bb8f7cfb17671fa9519db2d0285
Maky <franklinjaradev@gmail.com>
2021-06-06 14:13:57 -0400

    Data seeder to import/destroy dummy data

---
+++ server/seeder.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import users from "./data/users.js";
+import products from "./data/products.js";
+import User from "./models/userModel.js";
+import Product from "./models/productModel.js";
+import Order from "./models/orderModel.js";
+import connectDB from "./config/db.js";
+
+// This is to import data in case we delete it and to restore it
+
+dotenv.config();
+
+connectDB();
+
+const importData = async () => {
+  try {
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    const createdUsers = await User.insertMany(users);
+    // get the admin from users.js
+    const adminUser = createdUsers[0]._id;
+
+    const sampleProducts = products.map((product) => {
+      return { ...product, user: adminUser };
+    });
+
+    await Product.insertMany(sampleProducts);
+    console.log("Data Imported");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+const destroyData = async () => {
+  try {
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    console.log("Data Destroyed");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+// scripts for nodejs
+if (process.argv[2] === "-d") {
+  destroyData();
+} else {
+  importData();
+}
+
+export { importData, destroyData };
