5879d32f40c5aa8fb900b3c1908e29e820c76da2
Maky <franklinjaradev@gmail.com>
2021-06-06 20:41:26 -0400

    Cart Reducer, Action, Constant and CartScreen

--- client/src/store.js
+++ client/src/store.js
@@ -5,13 +5,24 @@ import {
   productListReducer,
   productDetailsReducer,
 } from "./reducers/productReducers";
+import { cartReducer } from "./reducers/cartReducers";
 
 const reducer = combineReducers({
+  // product
   productList: productListReducer,
   productDetails: productDetailsReducer,
+
+  // cart
+  cart: cartReducer,
 });
 
-const initialState = {};
+const cartItemsFromStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage },
+};
 
 const middleware = [thunk];
 
