6c36c9b8be3fedfc3f686e8c29ecfd677a16a242
Maky <franklinjaradev@gmail.com>
2021-06-10 11:34:43 -0400

    Generate a JSON Web Token for User Auth

---
+++ server/utils/generateToken.js
@@ -0,0 +1,10 @@
+import jwt from "jsonwebtoken";
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    // 30 days
+    expiresIn: "30d",
+  });
+};
+
+export default generateToken;
