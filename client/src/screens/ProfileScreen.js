59493cf15077d7a944662fcc33f7296658303975
Maky <franklinjaradev@gmail.com>
2021-06-16 15:53:24 -0400

    Updating User Profile

--- client/src/screens/ProfileScreen.js
+++ client/src/screens/ProfileScreen.js
@@ -5,7 +5,7 @@ import Message from "../components/Message";
 import Loader from "../components/Loader";
 
 // redux
-import { getUserDetails } from "../actions/userActions";
+import { getUserDetails, updateUserProfile } from "../actions/userActions";
 
 // form validation
 const ProfileScreen = ({ history }) => {
@@ -22,6 +22,9 @@ const ProfileScreen = ({ history }) => {
 
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
+
+  const userUpdateProfile = useSelector((state) => state.userUpdateProfile);
+  const { success } = userUpdateProfile;
 
   useEffect(() => {
     if (!userInfo) {
@@ -45,6 +48,14 @@ const ProfileScreen = ({ history }) => {
       setMessage("Passwords do not match");
     } else {
       // Dispatch UPDATE PROFILE
+      dispatch(
+        updateUserProfile({
+          id: user._id,
+          name,
+          email,
+          password,
+        })
+      );
     }
   };
 
@@ -55,6 +66,7 @@ const ProfileScreen = ({ history }) => {
 
         {message && <Message>{message}</Message>}
         {error && <Message>{error}</Message>}
+        {success && <Message variant="success">Profile Updated</Message>}
         {loading && <Loader />}
 
         <Form onSubmit={submitHandler}>
