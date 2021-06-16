09d0084626aec944f82e41bd9f60b563d106535d
Maky <franklinjaradev@gmail.com>
2021-06-16 16:27:13 -0400

    Shipping Screen and Save Address

--- client/src/store.js
+++ client/src/store.js
@@ -38,8 +38,16 @@ const userInfoFromStorage = localStorage.getItem("userInfo")
   ? JSON.parse(localStorage.getItem("userInfo"))
   : null;
 
+// shipping
+const shippingAddressFromStorage = localStorage.getItem("shippingAddress")
+  ? JSON.parse(localStorage.getItem("shippingAddress"))
+  : {};
+
 const initialState = {
-  cart: { cartItems: cartItemsFromStorage },
+  cart: {
+    cartItems: cartItemsFromStorage,
+    shippingAddress: shippingAddressFromStorage,
+  },
   userLogin: { userInfo: userInfoFromStorage },
 };
 
