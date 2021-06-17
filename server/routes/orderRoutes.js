6f0682930dc654766defe9ea0f49cebeda485a1f
Maky <franklinjaradev@gmail.com>
2021-06-16 20:27:17 -0400

    Getting an Order by ID - server

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,12 +1,16 @@
 import express from "express";
 
 // controllers
-import { addOrderItems } from "../controllers/orderControllers.js";
+import {
+  addOrderItems,
+  getOrderById,
+} from "../controllers/orderControllers.js";
 // middleware
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/").post(protect, addOrderItems);
+router.route("/:id").get(protect, getOrderById);
 
 export default router;
