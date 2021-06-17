7f6a0b212c42de7487edcc284443a14afb4e2da7
Maky <franklinjaradev@gmail.com>
2021-06-16 21:02:43 -0400

    Order Screen

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import ProfileScreen from "./screens/ProfileScreen";
 import ShippingScreen from "./screens/ShippingScreen";
 import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import OrderScreen from "./screens/OrderScreen";
 import { Container } from "react-bootstrap";
 
 const App = () => {
@@ -28,6 +29,7 @@ const App = () => {
           <Route path="/shipping" component={ShippingScreen} />
           <Route path="/payment" component={PaymentScreen} />
           <Route path="/placeorder" component={PlaceOrderScreen} />
+          <Route path="/order/:id" component={OrderScreen} />
         </Container>
       </main>
       <Footer />
