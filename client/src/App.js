4369833cd9c5a7ae3bfccfa9527e22d2b78a3038
Maky <franklinjaradev@gmail.com>
2021-06-18 18:55:08 -0400

    Admin User List Screen

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import ShippingScreen from "./screens/ShippingScreen";
 import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
+import UserListScreen from "./screens/UserListScreen";
 import { Container } from "react-bootstrap";
 
 const App = () => {
@@ -30,6 +31,7 @@ const App = () => {
           <Route path="/payment" component={PaymentScreen} />
           <Route path="/placeorder" component={PlaceOrderScreen} />
           <Route path="/order/:id" component={OrderScreen} />
+          <Route path="/admin/userlist" component={UserListScreen} />
         </Container>
       </main>
       <Footer />
