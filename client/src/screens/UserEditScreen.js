197a8c15fa16a300adbaeddcc97637829f0d7fa2
Maky <franklinjaradev@gmail.com>
2021-06-18 21:55:15 -0400

    Update User Functionallity for Admin Page

--- client/src/screens/UserEditScreen.js
+++ client/src/screens/UserEditScreen.js
@@ -7,7 +7,8 @@ import Message from "../components/Message";
 import Loader from "../components/Loader";
 
 // redux
-import { getUserDetails } from "../actions/userActions";
+import { getUserDetails, updateUser } from "../actions/userActions";
+import { USER_UPDATE_RESET } from "../constants/userConstants";
 
 // form validation
 const UserEditScreen = ({ match, history }) => {
@@ -20,21 +21,33 @@ const UserEditScreen = ({ match, history }) => {
   const dispatch = useDispatch();
 
   const userDetails = useSelector((state) => state.userDetails);
-
   const { loading, error, user } = userDetails;
 
+  const userUpdate = useSelector((state) => state.userUpdate);
+  const {
+    loading: loadingUpdate,
+    error: errorUpdate,
+    success: successUpdate,
+  } = userUpdate;
+
   useEffect(() => {
-    if (!user.name || user._id !== userId) {
-      dispatch(getUserDetails(userId));
+    if (successUpdate) {
+      dispatch({ type: USER_UPDATE_RESET });
+      history.push("/admin/userlist");
     } else {
-      setName(user.name);
-      setEmail(user.email);
-      setIsAdmin(user.isAdmin);
+      if (!user.name || user._id !== userId) {
+        dispatch(getUserDetails(userId));
+      } else {
+        setName(user.name);
+        setEmail(user.email);
+        setIsAdmin(user.isAdmin);
+      }
     }
-  }, [dispatch, userId, user]);
+  }, [dispatch, userId, user, successUpdate, history]);
 
   const submitHandler = (e) => {
     e.preventDefault();
+    dispatch(updateUser({ _id: userId, name, email, isAdmin }));
   };
 
   return (
@@ -45,6 +58,9 @@ const UserEditScreen = ({ match, history }) => {
 
       <FormContainer>
         <h1 className="text-center">Edit User</h1>
+
+        {loadingUpdate && <Loader />}
+        {errorUpdate && <Message>{errorUpdate}</Message>}
 
         {loading ? (
           <Loader />
