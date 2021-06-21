4866643768d9c88645340d9a9fb4aaee43f5ed1b
Maky <franklinjaradev@gmail.com>
2021-06-21 18:01:19 -0400

    Image Upload Config and Endpoint

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
@@ -26,11 +28,17 @@ app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 // order routes
 app.use("/api/orders", orderRoutes);
+// uploads
+app.use("/api/upload", uploadRoutes);
 
 // paypal
 app.get("/api/config/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID);
 });
+
+// Static folder
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 // middleware and custom error handler
 app.use(notFound);
